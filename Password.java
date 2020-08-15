0a81975216186f55ab6812bae23fe45098ec666f
saahilvasdev <63179495+saahilvasdev@users.noreply.github.com>
2020-08-15 16:37:09 -0700

    Add files via upload

--- Password.java
+++ Password.java
@@ -43,13 +43,36 @@ public class Password {
 		/*
 		 * Password generator
 		 */
-		public void GeneratePassword(boolean letters, boolean upper, boolean lower, boolean numbers, boolean symbols, int length) {
+		public String GeneratePassword(boolean letters, boolean upper, boolean lower, boolean numbers, boolean symbols, int length) {
 			//generate password based on specifications of user
 			String generatedPass = "";
 			
 			//do the generating thing
 			
-			this.pass = generatedPass;
+			for(int i = 0; i < length - 2; i++) {
+			generatedPass = generatedPass + randomCharacter("abcdefghijklmnopqrstuvwxyz");
+			}
+			
+			
+			String randomDigit = randomCharacter("0123456789");
+			generatedPass = insertAtRandom(generatedPass, randomDigit);
+			
+			String randomSymbs = randomCharacter("+-*/?!@#$%^&()");
+			generatedPass = insertAtRandom(generatedPass, randomSymbs);
+			
+			return generatedPass;
+		}
+		
+		public String randomCharacter(String characters) {
+			int n = characters.length();
+			int r = (int) (n * Math.random());
+			return characters.substring(r, r + 1);
+		}
+		
+		public String insertAtRandom(String str, String toInsert) {
+			int n = str.length();
+			int r = (int) ((n + 1) * Math.random());
+			return str.substring(0,r) + toInsert + str.substring(r);
 		}
 		
 		/*
