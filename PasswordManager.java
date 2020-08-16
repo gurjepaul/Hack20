605e957d039b99bf217858eada360bf875e9df8a
Gurjepaul <47045111+gurjepaul@users.noreply.github.com>
2020-08-15 19:57:24 -0700

    Add files via upload

--- PasswordManager.java
+++ PasswordManager.java
@@ -1,3 +1,7 @@
+import java.math.BigInteger;  
+import java.nio.charset.StandardCharsets; 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
 /**
@@ -10,67 +14,153 @@ import java.util.HashMap;
  */
 public class PasswordManager
 {
-	private String masterKey;
+	private final String masterKey;
 	private HashMap<String, String> container;
 	
-	public PasswordManager()
+	public PasswordManager(String masterKey) throws NoSuchAlgorithmException
 	{
-		this.masterKey = this.setupMasterKey();
+		this.masterKey = this.toHexString(this.getSHA(masterKey));
 		this.container = new HashMap<>();
 	}
 	
-	private String setupMasterKey()
+	private byte[] getSHA(String input) throws NoSuchAlgorithmException
 	{
-		// user password strength checker
-		// set this.masterKey to hash of password inputed
-		return null;
+		// Static getInstance method is called with hashing SHA  
+        MessageDigest md = MessageDigest.getInstance("SHA-256");  
+  
+        // digest() method called  
+        // to calculate message digest of an input  
+        // and return array of byte 
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));
 	}
 	
-	private String getHash(String sequence)
+	public String toHexString(byte[] hash)
 	{
-		// uses hashing algorithm to return a hash of a string
-		return null;
+		// Convert byte array into signum representation  
+        BigInteger number = new BigInteger(1, hash);  
+  
+        // Convert message digest into hex value  
+        StringBuilder hexString = new StringBuilder(number.toString(16));  
+  
+        // Pad with leading zeros 
+        while (hexString.length() < 32)  
+        {  
+            hexString.insert(0, '0');  
+        }  
+  
+        return hexString.toString();  
 	}
 	
-	private boolean isMasterKey(String key)
+	public boolean isMasterKey(String key) throws NoSuchAlgorithmException
 	{
-		// returns true if the hash of the key equals the hash of the masterkey
-		return (this.getHash(key)).equals(this.masterKey);
+		// returns true if the hash of the key equals the hash of the master key
+		return (this.toHexString(this.getSHA(key))).equals(this.masterKey);
 	}
 	
 	public boolean addPassword(String account, String password)
 	{
-		// compare and see if there is already a key for that
-		// if there is update ? or return false
-		// if not add new key and password to hashmap
-	    // return true
+		// Check if container contains the account
+		if (container.containsKey(account))
+		{
+			return false;
+		}
+		
+		String encrypted = AES.encrypt(password, this.masterKey);
+		
+		// Adding new account and password to HashMap
+		container.put(account, encrypted);
+	    
 		return true;
 	}
 	
-	public String getPassword(String account)
+	public boolean updatePassword(String account, String password)
 	{
-		// ask to input account name (key)
-		// check if key is in hashmap
-		// if it is then ask user to put in masterkey
-		// if valid then display the password
-		return null;
+		// Check if container doesn't contain the account
+		if (!container.containsKey(account))
+		{
+			return false;
+		}
+		
+		String encrypted = AES.encrypt(password, this.masterKey);
+		
+		// Updates the password of the account in the HashMap
+		container.replace(account, encrypted);
+		
+		return true;
+	}
+	
+	public String retrievePassword(String account)
+	{
+		// Check if container doens't contain the account
+		if (!container.containsKey(account))
+		{
+			return null;
+		}
+		
+		String decrypted = AES.decrypt((String)container.get(account), this.masterKey);
+		
+		// Return the password of the account
+		return decrypted;
+	}
+	
+	public boolean removePassword(String account)
+	{
+		// Check if container doesn't contain the account
+		if (!container.containsKey(account))
+		{
+			return false;
+		}
+		
+		// Remove the account from the HashMap
+		container.remove(account);
+		
+		return true;
 	}
 	
 	@Override
 	public String toString()
 	{
-		// outputs all the account names on the left and asterisks for 
-		// passwords on the right
-		return null;
+		StringBuilder accounts = new StringBuilder();
+		
+		for (String accountName : this.container.keySet())
+		{
+			accounts.append(String.format("%-12s: *******\n", accountName));
+		}
+		
+		return accounts.toString();
 	}
 
 	/**
 	 * @param args
+	 * @throws NoSuchAlgorithmException 
 	 */
-	public static void main(String[] args)
+	public static void main(String[] args) throws NoSuchAlgorithmException
 	{
-		// TODO Auto-generated method stub
-
+//		PasswordManager pm = new PasswordManager("Jay");
+//		
+//		System.out.println(pm.isMasterKey("Jay"));
+//		System.out.println(pm.isMasterKey("Saahil"));
+//		System.out.println(pm.isMasterKey("Jay"));
+//		
+//		System.out.println(pm.addPassword("Google", "123456"));
+//		System.out.println(pm.addPassword("Google", "jay"));
+//		
+//		System.out.println(pm.updatePassword("Google", "jay"));
+//		
+//		System.out.println(pm.updatePassword("Spotify", "jay"));
+//		
+//		System.out.println(pm.addPassword("Microsoft", "566969"));
+//		System.out.println(pm.addPassword("Apple", "Password"));
+//		System.out.println(pm.addPassword("Steam", "Password"));
+//		
+//		System.out.println(pm.removePassword("Spotify"));
+//		System.out.println(pm.removePassword("Google"));
+//		
+//		System.out.println(pm.retrievePassword("Google"));
+//		System.out.println(pm.retrievePassword("Microsoft"));
+//		System.out.println(pm.retrievePassword("Apple"));
+//		
+//		System.out.println(pm.toString());
 	}
 
 }
