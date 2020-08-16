bddbf911b02a59944d02bd8d810aa47be14f59d5
Jose Palomera <56489514+joseapalomera@users.noreply.github.com>
2020-08-16 03:32:24 -0700

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -1,22 +1,49 @@
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class Driver {
 
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchAlgorithmException {
 		// TODO Auto-generated method stub
 
-		System.out.println("Welcome to the password generator");
-		
-		boolean again = true;	//Boolean variable that will be true if the user wants to do something else
-
 		Scanner scan = new Scanner(System.in);
+	
+		char repeat;
+		
+		System.out.println("Welcome to the Password Manager");
+		System.out.println("Enter a master key to get started...");
+	
+		String masterKey = scan.nextLine();
+		PasswordManager pm = new PasswordManager(masterKey);
+	
+		boolean keyRight = false;
+	
+		while(keyRight == false)
+		{
+			
+			System.out.println("Enter the master key to access functions: ");
+			String key = scan.nextLine();
+			
+			if(pm.isMasterKey(key))
+			{
+				break;
+			}
+			else
+			{
+				System.out.println("Key is wrong, try again");
+			}
+		}
+		
+
+		boolean again = true;	//Boolean variable that will be true if the user wants to do something else
 		
 		while(again == true)
 		{
 			
 		System.out.println("-Type 'Create' for a new password, \n-Type 'Update' to update a password");
-		System.out.println("-Type 'Delete' to delete a password, \n-Type 'Retrieve' to get a password, or type 'Exit' ");
+		System.out.println("-Type 'Delete' to delete a password, \n-Type 'Retrieve' to get a password");
+		System.out.println("-Type 'Add' to add a password to your list, \n-Type 'View' to view a list of your passwords");
+		System.out.println("-Type 'Exit' to exit the password manager");
 		
 		
 		String word = scan.nextLine();
@@ -24,10 +51,10 @@ public class Driver {
 		System.out.println("------------------------------------");
 
 			//Determine what the user wants to do with the program
-			switch(word)
+			switch(word.toLowerCase())
 			{
 			//Case where the user wants to create a password
-			case "Create":
+			case "create":
 				
 				System.out.println("Enter 'New' to type your own password, or 'Generate' for a randomly generated password ");
 				
@@ -124,7 +151,7 @@ public class Driver {
 				break;
 		
 			//Case where the user wants to update a password
-			case "Add":
+			case "add":
 				System.out.println("Enter your master key");
 				if(pm.isMasterKey(scan.nextLine())) {
 					System.out.println("Enter the account you would like to update");
@@ -144,23 +171,23 @@ public class Driver {
 				
 				
 				System.out.println("Would you like to continue to use the password manager? (Y/N)");
-				char repeat = scan.next().toUpperCase().charAt(0);
+				 repeat = scan.nextLine().toUpperCase().charAt(0);
 				if (repeat == 'N') {
 					again = false;
 				}
 				
 				break;
 				
-			case "View":	
+			case "view":	
 				System.out.println("Enter your master key");
 				if(pm.isMasterKey(scan.nextLine())) {
-					pm.toString();
+					System.out.println(pm.toString());
 				} else {
 					System.out.println("Invalid Master key");
 				}
 				
 				System.out.println("Would you like to continue to use the password manager? (Y/N)");
-				char repeat = scan.next().toUpperCase().charAt(0);
+				 repeat = scan.nextLine().toUpperCase().charAt(0);
 				if (repeat == 'N') {
 					again = false;
 				}
@@ -168,7 +195,7 @@ public class Driver {
 				break;
 				
 				
-			case "Update":
+			case "update":
 			
 				System.out.println("Enter your master key");
 				if(pm.isMasterKey(scan.nextLine())) {
@@ -191,7 +218,7 @@ public class Driver {
 				//Once we access the passwords, the user chooses an account to change the password
 				//returns whether the password is strong, weak ,or okay
 				System.out.println("Would you like to continue to use the password manager? (Y/N)");
-				char repeat = scan.next().toUpperCase().charAt(0);
+				 repeat = scan.nextLine().toUpperCase().charAt(0);
 				if (repeat == 'N') {
 					again = false;
 				}
@@ -201,7 +228,7 @@ public class Driver {
 				break;
 			
 			//Case where the user wants to delete a password
-			case "Delete":
+			case "delete":
 				
 				System.out.println("Enter your master key");
 				if(pm.isMasterKey(scan.nextLine())) {
@@ -218,7 +245,7 @@ public class Driver {
 				}
 				
 				System.out.println("Would you like to continue to use the password manager? (Y/N)");
-				char repeat = scan.next().toUpperCase().charAt(0);
+				 repeat = scan.nextLine().toUpperCase().charAt(0);
 				if (repeat == 'N') {
 					again = false;
 				}
@@ -232,7 +259,7 @@ public class Driver {
 				break;
 			
 				//Case where the user wants to retrieve a password
-			case "Retrieve":
+			case "retrieve":
 				System.out.println("Enter your master key");
 				
 				if(pm.isMasterKey(scan.nextLine())) {
@@ -251,7 +278,7 @@ public class Driver {
 				//return the password as a string to the user
 				
 				System.out.println("Would you like to continue to use the password manager? (Y/N)");
-				char repeat = scan.next().toUpperCase().charAt(0);
+				 repeat = scan.nextLine().toUpperCase().charAt(0);
 				if (repeat == 'N') {
 					again = false;
 				}
@@ -260,7 +287,7 @@ public class Driver {
 				break;
 			
 			//User wants to exit the program
-			case "Exit":
+			case "exit":
 				
 				System.out.println("Exiting the password manager...");
 			
@@ -280,7 +307,7 @@ public class Driver {
 		}
 		
 		scan.close();
-		
+		System.out.println("Exiting the password manager...");
 	}
 	
 }
